881a976afd9ba1fa76d8183c391f5dbb0b91e319
gbarber3 <gabriel.r.barber@gmail.com>
2020-01-15 13:01:03 -0700

    First App Dev Exercise for Objects

---
+++ AppDevObjEx1/AppDevObjEx1/Course.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppDevObjEx1
+{
+    class Course
+    {
+        int courseNumber;
+        string courseName;
+
+        public Course()
+        {
+            courseNumber = 1501;
+            courseName = "Introduction to App Dev";
+        }
+        public Course (int _courseNumber)
+        {
+            courseNumber = _courseNumber;
+            courseName = "Introduction to Objects";
+        }
+
+        public void Writer()
+        {
+            Console.WriteLine($"The course number is \"{courseNumber}\"");
+            Console.WriteLine($"The course name is \"{courseName}\"");
+
+        }
+
+    }
+}
---
+++ AppDevObjEx1/AppDevObjEx1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AppDevObjEx1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Course objectOne = new Course();
+            objectOne.Writer();
+
+            Course objectTwo = new Course(1550);
+            objectTwo.Writer();
+        }
+    }
+}
